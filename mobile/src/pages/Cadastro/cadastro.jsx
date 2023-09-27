54f091b7c7d28eaa3faad53423238d52032acbec
Bernardo-Mattos-HomePc <bernardomtt2@gmail.com>
2023-09-27 00:12:24 -0300

    adicionando o icon

--- mobile/src/pages/Cadastro/cadastro.jsx
+++ mobile/src/pages/Cadastro/cadastro.jsx
@@ -3,29 +3,26 @@ import {
   Text,
   StyleSheet,
   TextInput,
-  TouchableOpacity,
   Modal,
   Pressable,
-  Alert
 } from "react-native";
 import { useState } from "react";
 import saveCadastroData from "../../config/firebase.config";
+import { FontAwesome } from "@expo/vector-icons";
 
-export default function Cadastro({navigation}) {
+export default function Cadastro({ navigation }) {
   // gera um delay em algumas functions
-  const delay = ms => new Promise(
-    resolve => setTimeout(resolve, ms)
-  );
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-  const [name , setName ] = useState('');
-  const [age , setAge ] = useState('');
-  const [email , setEmail ] = useState('');
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [email, setEmail] = useState("");
   const [modalVisible, setModalVisible] = useState(false);
-  
-  const  handleSignUp = async () => {
-    saveCadastroData(name , age , email);
+
+  const handleSignUp = async () => {
+    saveCadastroData(name, age, email);
     await delay(2000);
-    navigation.navigate('Login');
+    navigation.navigate("Login");
   };
 
   return (
@@ -36,29 +33,49 @@ export default function Cadastro({navigation}) {
         visible={modalVisible}
         onRequestClose={() => {
           setModalVisible(!modalVisible);
-        }}>
+        }}
+      >
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <Text style={styles.modalText}>Usuario cadastrado com sucesso !</Text>
+            <Text style={styles.modalText}>
+              Usuario cadastrado com sucesso !
+            </Text>
             <Pressable
               style={[styles.button, styles.buttonClose]}
-              onPress={handleSignUp}>
+              onPress={handleSignUp}
+            >
               <Text style={styles.textStyle}>Logar</Text>
             </Pressable>
           </View>
         </View>
       </Modal>
-      <Text style={styles.ttl}>cadastro page</Text>
       <View style={styles.containerInput}>
-        <TextInput style={styles.input} placeholder="Name" onChangeText={(text) => setName(text)}/>
-        <TextInput style={styles.input} placeholder="Age" onChangeText={(text) => setAge(text)}/>
-        <TextInput style={styles.input} placeholder="Email" onChangeText={(text) => setEmail(text)}/>
-      </View>
-      <Pressable
+        <FontAwesome name="user" size={100} color="#999" />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Name"
+          onChangeText={(text) => setName(text)}
+        />
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          placeholder="Age"
+          onChangeText={(text) => setAge(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Pressable
         style={[styles.button, styles.buttonOpen]}
-        onPress={() => setModalVisible(true)}>
+        onPress={() => setModalVisible(true)}
+      >
         <Text style={styles.textStyle}>Cadastrar</Text>
       </Pressable>
+      </View>
+      
     </View>
   );
 }
@@ -67,9 +84,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     display: "flex",
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
-    backgroundColor: "#444",
+    backgroundColor: "#fff",
   },
   ttl: {
     color: "#fff",
@@ -77,14 +94,16 @@ const styles = StyleSheet.create({
   containerInput: {
     padding: 20,
     width: "80%",
+    height: "70%",
     display: "flex",
     alignItems: "center",
-    justifyContent: "space-around",
+    justifyContent: "flex-start",
+    top:50,
   },
   input: {
     width: "90%",
     borderWidth: 1,
-    borderColor: "#fff",
+    borderColor: "#000",
     borderRadius: 4,
     height: 55,
     marginTop: 10,
@@ -93,11 +112,11 @@ const styles = StyleSheet.create({
 
   modalView: {
     margin: 20,
-    backgroundColor: '#0a5c36',
+    backgroundColor: "#0a5c36",
     borderRadius: 20,
     padding: 20,
-    alignItems: 'center',
-    shadowColor: '#000',
+    alignItems: "center",
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -108,24 +127,25 @@ const styles = StyleSheet.create({
   },
   button: {
     borderRadius: 4,
-    padding: 10.20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
     elevation: 1,
-
   },
   buttonOpen: {
-    backgroundColor: '#888',
+    backgroundColor: "#888",
+    top: 20,
   },
   buttonClose: {
-    backgroundColor: '#2196F3',
+    backgroundColor: "#888",
   },
   textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
   },
   modalText: {
     marginBottom: 15,
-    textAlign: 'center',
-    color: '#fff'
+    textAlign: "center",
+    color: "#fff",
   },
 });
